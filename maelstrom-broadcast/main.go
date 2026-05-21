65602c8334dc169afd60cdc04d84c1bd3679c49f
Seyoung Kim <seyoungjkim@gmail.com>
2026-05-21 14:27:21 -0400

    3b working finally

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -3,30 +3,53 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-// Run: ./../maelstrom/maelstrom test -w broadcast --bin ~/go/bin/maelstrom-broadcast --node-count 1 --time-limit 20 --rate 10
-// Run: ./../maelstrom/maelstrom test -w broadcast --bin ~/go/bin/maelstrom-broadcast --node-count 5 --time-limit 20 --rate 10
+// Run 3a: ./../maelstrom/maelstrom test -w broadcast --bin ~/go/bin/maelstrom-broadcast --node-count 1 --time-limit 20 --rate 10
+// Run 3b: ./../maelstrom/maelstrom test -w broadcast --bin ~/go/bin/maelstrom-broadcast --node-count 5 --time-limit 20 --rate 10
+
+type broadcastMessage struct {
+	Type    string `json:"type"`
+	Message int64  `json:"message"`
+}
+
+type topologyMessage struct {
+	Type     string              `json:"type"`
+	Topology map[string][]string `json:"topology"`
+}
 
 func main() {
 	n := maelstrom.NewNode()
-	var messages []any
-	var topology map[string][]string
+	mu := &sync.Mutex{}
+	seenMessages := make(map[int64]struct{})
+	var messageList []int64
+	topology := make(map[string][]string)
 
 	// Requests node broadcast message
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
-		// Unmarshal the message body as a loosely-typed map.
-		var body map[string]any
+		// Unmarshal the message body
+		var body broadcastMessage
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
-		messages = append(messages, body["message"])
-		// TODO: figure out how to efficiently broadcast seen messages. Prevent loops.
+		_, ok := seenMessages[body.Message]
+		if ok {
+			return n.Reply(msg, map[string]any{"type": "broadcast_ok"})
+		}
+		// Only send new messages to neighbors
+		mu.Lock()
+		seenMessages[body.Message] = struct{}{}
+		messageList = append(messageList, body.Message)
+		mu.Unlock()
 		for _, neighbor := range topology[n.ID()] {
-			err := n.Send(neighbor, body)
+			if neighbor == msg.Src {
+				continue
+			}
+			err := n.RPC(neighbor, body, func(msg maelstrom.Message) error { return nil })
 			if err != nil {
 				return err
 			}
@@ -36,30 +59,17 @@ func main() {
 
 	// Asks node to return seen messages
 	n.Handle("read", func(msg maelstrom.Message) error {
-		// Unmarshal the message body as a loosely-typed map.
-		var body map[string]any
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		return n.Reply(msg, map[string]any{"type": "read_ok", "messages": messages})
+		return n.Reply(msg, map[string]any{"type": "read_ok", "messages": messageList})
 	})
 
 	// Informs nodes of neighbors
 	n.Handle("topology", func(msg maelstrom.Message) error {
-		// Unmarshal the message body as a loosely-typed map.
-		var body map[string]any
+		// Unmarshal the message body
+		var body topologyMessage
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-		topologyBytes, err := json.Marshal(topology["body"])
-		if err != nil {
-			return err
-		}
-		if err := json.Unmarshal(topologyBytes, &topology); err != nil {
-			return err
-		}
-
+		topology = body.Topology
 		return n.Reply(msg, map[string]any{"type": "topology_ok"})
 	})
 
