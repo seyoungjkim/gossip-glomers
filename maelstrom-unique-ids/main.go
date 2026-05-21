b68a89df1e4a5dbc55c808cd67c1d3981a4df736
Seyoung Kim <seyoungjkim@gmail.com>
2026-05-21 14:42:00 -0400

    fix id

--- maelstrom-unique-ids/main.go
+++ maelstrom-unique-ids/main.go
@@ -3,16 +3,16 @@ package main
 import (
 	"encoding/json"
 	"log"
-	"strconv"
 
+	"github.com/google/uuid"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 // Run: ./../maelstrom/maelstrom test -w unique-ids --bin ~/go/bin/maelstrom-unique-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+// Consider other IDs, such as Snowflake ID: https://en.wikipedia.org/wiki/Snowflake_ID
 
 func main() {
 	n := maelstrom.NewNode()
-	count := uint64(0)
 	n.Handle("generate", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as a loosely-typed map.
 		var body map[string]any
@@ -23,9 +23,8 @@ func main() {
 		// Send message back with UUID.
 		newBody := map[string]any{
 			"type": "generate_ok",
-			"id":   n.ID() + "-" + strconv.FormatUint(count, 10),
+			"id":   uuid.New(),
 		}
-		count += 1
 		return n.Reply(msg, newBody)
 	})
 	if err := n.Run(); err != nil {
