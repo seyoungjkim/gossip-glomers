c06ebeab8618e0149a25b24f96287bdb61ad1e26
Seyoung Kim <seyoungjkim@gmail.com>
2026-06-01 14:06:17 -0400

    new approach to ch4

--- maelstrom-counter/main.go
+++ maelstrom-counter/main.go
@@ -5,26 +5,19 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
-	"sync"
-	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 // Run: ./../maelstrom/maelstrom test -w g-counter --bin ~/go/bin/maelstrom-counter --node-count 3 --rate 100 --time-limit 20 --nemesis partition
 
-const retryBackoff = 5 * time.Millisecond
-const valueKey = "value"
-
 type addMessage struct {
 	Delta int `json:"delta"`
 }
 
 func main() {
-	var mu sync.Mutex
 	n := maelstrom.NewNode()
 	kv := maelstrom.NewSeqKV(n)
-	nodeDelta := 0
 
 	// Helper function to return key value as int, 0 otherwise
 	readIfExists := func(key string) (int, error) {
@@ -49,59 +42,29 @@ func main() {
 		delta := body.Delta
 
 		// Read old value and add the delta
-		value, err := readIfExists(valueKey)
+		value, err := readIfExists(n.ID())
 		if err != nil {
 			return err
 		}
 		newValue := value + delta
-		err = kv.CompareAndSwap(context.Background(), valueKey, value, newValue, true)
+		err = kv.CompareAndSwap(context.Background(), n.ID(), value, newValue, true)
 		if err != nil {
-			rpcErr, ok := errors.AsType[*maelstrom.RPCError](err)
-			// The counter was updated - schedule another attempt
-			if ok && rpcErr.Code == maelstrom.PreconditionFailed {
-				mu.Lock()
-				nodeDelta += delta
-				mu.Unlock()
-			} else {
-				return err
-			}
+			return err
 		}
 		return n.Reply(msg, map[string]any{"type": "add_ok"})
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
-		value, err := readIfExists(valueKey)
-		if err != nil {
-			return err
+		value := 0
+		for _, node := range n.NodeIDs() {
+			nodeValue, err := readIfExists(node)
+			if err != nil {
+				return err
+			}
+			value += nodeValue
 		}
 		return n.Reply(msg, map[string]any{"type": "read_ok", "value": value})
 	})
-
-	// Background goroutine to retry adding periodically
-	go func() {
-		ticker := time.NewTicker(retryBackoff)
-		for range ticker.C {
-			// Skip if nothing to add
-			mu.Lock()
-			currDelta := nodeDelta
-			mu.Unlock()
-			if currDelta == 0 {
-				continue
-			}
-			// Read old value and add the delta
-			value, err := readIfExists(valueKey)
-			if err != nil {
-				continue
-			}
-			newValue := value + currDelta
-			err = kv.CompareAndSwap(context.Background(), valueKey, value, newValue, true)
-			if err == nil {
-				mu.Lock()
-				nodeDelta -= currDelta
-				mu.Unlock()
-			}
-		}
-	}()
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
