cbbe961e158478d436520dfb6938d3c7af4213bf
Seyoung Kim <seyoungjkim@gmail.com>
2026-06-11 18:42:10 -0400

    optimize logs

--- maelstrom-kafka-5c/main.go
+++ maelstrom-kafka-5c/main.go
@@ -7,14 +7,13 @@ import (
 	"fmt"
 	"hash/fnv"
 	"log"
-	"strconv"
 	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 // Run: ./../maelstrom/maelstrom test -w kafka --bin ~/go/bin/maelstrom-kafka-5c --node-count 2 --concurrency 2n --time-limit 20 --rate 1000
-// 5b Result:
+// 5b result (1 msg/poll):
 //       :availability {:valid? true, :ok-fraction 0.99964577},
 //       :net {:all {:send-count 382318,
 //             :recv-count 382318,
@@ -28,7 +27,21 @@ import (
 //                 :msg-count 334148,
 //                 :msgs-per-op 19.72655},
 //       :valid? true},
-// 5c Result after optimizing client offsets:
+// 5b result (5 msg/poll)
+//       :availability {:valid? true, :ok-fraction 0.9995857},
+//       :net {:all {:send-count 259414,
+//             :recv-count 259414,
+//             :msg-count 259414,
+//             :msgs-per-op 15.353575},
+//       :clients {:send-count 43188,
+//                 :recv-count 43188,
+//                 :msg-count 43188},
+//       :servers {:send-count 216226,
+//                 :recv-count 216226,
+//                 :msg-count 216226,
+//                 :msgs-per-op 12.797467},
+//       :valid? true},
+// 5c result after optimizing client offsets (1 msg/poll):
 //       :availability {:valid? true, :ok-fraction 0.99953276},
 //       :net {:all {:send-count 261346,
 //             :recv-count 261346,
@@ -42,6 +55,36 @@ import (
 //                 :msg-count 213028,
 //                 :msgs-per-op 12.441771},
 //       :valid? true},
+// 5c result after optimizing client offsets and logs (5 msg/poll):
+//       :availability {:valid? true, :ok-fraction 0.9995894},
+//       :net {:all {:send-count 169266,
+//             :recv-count 169266,
+//             :msg-count 169266,
+//             :msgs-per-op 9.929372},
+//       :clients {:send-count 43658,
+//                 :recv-count 43658,
+//                 :msg-count 43658},
+//       :servers {:send-count 125608,
+//                 :recv-count 125608,
+//                 :msg-count 125608,
+//                 :msgs-per-op 7.368335},
+//       :valid? true},
+// Same with 10 msg/poll:
+//       :availability {:valid? true, :ok-fraction 0.9995302},
+//       :net {:all {:send-count 152760,
+//             :recv-count 152760,
+//             :msg-count 152760,
+//             :msgs-per-op 8.970579},
+//       :clients {:send-count 42460,
+//                 :recv-count 42460,
+//                 :msg-count 42460},
+//       :servers {:send-count 110300,
+//                 :recv-count 110300,
+//                 :msg-count 110300,
+//                 :msgs-per-op 6.477186},
+//       :valid? true},
+
+const pollMessageCount = 10
 
 const logPrefix = "log-"
 const offsetPrefix = "offset-"
@@ -72,7 +115,9 @@ type sendInternalMessage struct {
 type server struct {
 	n              *maelstrom.Node
 	kv             *maelstrom.KV
-	mu             *sync.Mutex
+	keyMutex       *sync.Mutex
+	logMutex       *sync.Mutex
+	clientMutex    *sync.Mutex
 	messageOffsets map[string]int
 }
 
@@ -82,6 +127,8 @@ func main() {
 	s := server{
 		n,
 		kv,
+		&sync.Mutex{},
+		&sync.Mutex{},
 		&sync.Mutex{},
 		make(map[string]int),
 	}
@@ -142,13 +189,21 @@ func main() {
 		}
 		msgs := make(map[string][][]int)
 		for requestedKey, requestedOffset := range body.Offsets {
-			requestedLog, err := s.readIntIfExists(formatLogKey(requestedKey, requestedOffset))
+			requestedLogs, err := s.readLogsIfExists(logPrefix + requestedKey)
 			if err != nil {
 				return err
 			}
-			if requestedLog != nil {
-				// Return message at offset
-				msgs[requestedKey] = [][]int{{requestedOffset, *requestedLog}}
+			// Grab up to pollMessageCount logs
+			var logs [][]int
+			for i := 0; i < pollMessageCount; i++ {
+				requestedMessage, ok := requestedLogs[requestedOffset+i]
+				if !ok {
+					break
+				}
+				logs = append(logs, []int{requestedOffset + i, requestedMessage})
+			}
+			if len(logs) > 0 {
+				msgs[requestedKey] = logs
 			}
 		}
 		return n.Reply(msg, map[string]any{"type": "poll_ok", "msgs": msgs})
@@ -204,7 +259,7 @@ func main() {
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-		offsets, err := s.readMapIfExists(clientPrefix + msg.Src)
+		offsets, err := s.readOffsetsIfExists(clientPrefix + msg.Src)
 		if err != nil {
 			return err
 		}
@@ -230,9 +285,23 @@ func (s *server) readIntIfExists(key string) (*int, error) {
 }
 
 // Helper function to get key value; returns empty map if value doesn't exist
-func (s *server) readMapIfExists(key string) (map[string]int, error) {
+func (s *server) readOffsetsIfExists(key string) (map[string]int, error) {
 	m := map[string]int{}
-	err := s.kv.ReadInto(context.Background(), key, m)
+	err := s.kv.ReadInto(context.Background(), key, &m)
+	if err != nil {
+		rpcErr, ok := errors.AsType[*maelstrom.RPCError](err)
+		if ok && rpcErr.Code == maelstrom.KeyDoesNotExist {
+			return m, nil
+		}
+		return nil, err
+	}
+	return m, nil
+}
+
+// Helper function to get key value; returns empty map if value doesn't exist
+func (s *server) readLogsIfExists(key string) (map[int]int, error) {
+	m := map[int]int{}
+	err := s.kv.ReadInto(context.Background(), key, &m)
 	if err != nil {
 		rpcErr, ok := errors.AsType[*maelstrom.RPCError](err)
 		if ok && rpcErr.Code == maelstrom.KeyDoesNotExist {
@@ -255,7 +324,7 @@ func (s *server) routeNode(key string) (string, error) {
 
 func (s *server) handleSend(key string, message int) (int, error) {
 	// Read previous offset from memory, falling back to KV if needed
-	s.mu.Lock()
+	s.keyMutex.Lock()
 	_, ok := s.messageOffsets[key]
 	if !ok { // Grab previous offset
 		prevOffset, err := s.kv.ReadInt(context.Background(), offsetPrefix+key)
@@ -264,7 +333,7 @@ func (s *server) handleSend(key string, message int) (int, error) {
 			if ok && rpcErr.Code == maelstrom.KeyDoesNotExist {
 				prevOffset = -1
 			} else {
-				s.mu.Unlock()
+				s.keyMutex.Unlock()
 				return -1, err
 			}
 		}
@@ -272,14 +341,23 @@ func (s *server) handleSend(key string, message int) (int, error) {
 	}
 	s.messageOffsets[key]++
 	messageOffset := s.messageOffsets[key]
-	s.mu.Unlock()
+	s.keyMutex.Unlock()
 
 	// Write new offset - should not need CaS since each node has own key
 	err := s.kv.Write(context.Background(), offsetPrefix+key, messageOffset)
 	if err != nil {
 		return -1, err
 	}
-	err = s.kv.Write(context.Background(), formatLogKey(key, messageOffset), message)
+
+	// Update logs by reading existing and adding new
+	s.logMutex.Lock()
+	defer s.logMutex.Unlock()
+	logs, err := s.readLogsIfExists(logPrefix + key)
+	if err != nil {
+		return -1, err
+	}
+	logs[messageOffset] = message
+	err = s.kv.Write(context.Background(), logPrefix+key, logs)
 	if err != nil {
 		return -1, err
 	}
@@ -288,10 +366,11 @@ func (s *server) handleSend(key string, message int) (int, error) {
 
 // Read and write offsets from "client-{CLIENT_NAME}" key
 func (s *server) handleCommitOffsets(newOffsets map[string]int, client string) error {
-	s.mu.Lock()
-	clientOffsets, err := s.readMapIfExists(clientPrefix + client)
+	s.clientMutex.Lock()
+	defer s.clientMutex.Unlock()
+
+	clientOffsets, err := s.readOffsetsIfExists(clientPrefix + client)
 	if err != nil {
-		s.mu.Unlock()
 		return err
 	}
 	for logKey, logOffset := range newOffsets {
@@ -299,13 +378,7 @@ func (s *server) handleCommitOffsets(newOffsets map[string]int, client string) e
 	}
 	err = s.kv.Write(context.Background(), clientPrefix+client, &clientOffsets)
 	if err != nil {
-		s.mu.Unlock()
 		return err
 	}
-	s.mu.Unlock()
 	return nil
-}
-
-func formatLogKey(key string, offset int) string {
-	return logPrefix + key + "-" + strconv.Itoa(offset)
 }
