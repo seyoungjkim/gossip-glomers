e1d6414f4a5e2106b550688c453fd588d5974687
Seyoung Kim <seyoungjkim@gmail.com>
2026-06-18 12:28:53 -0400

    fix some non transactional stuff

--- maelstrom-txn-6b/main.go
+++ maelstrom-txn-6b/main.go
@@ -12,25 +12,24 @@ import (
 //   ./../maelstrom/maelstrom test -w txn-rw-register --bin ~/go/bin/maelstrom-txn-6b --node-count 2 --concurrency 2n --time-limit 20 --rate 1000 --consistency-models read-uncommitted
 // Run with partition:
 //   ./../maelstrom/maelstrom test -w txn-rw-register --bin ~/go/bin/maelstrom-txn-6b --node-count 2 --concurrency 2n --time-limit 20 --rate 1000 --consistency-models read-uncommitted --availability total --nemesis partition
-// TODO: this workload actually passes even on 6a...probably because no detection is actually happening.
-// TODO: handle retries
+// Note: These tests pass without implementing retries. Even 6a passes these tests.
 
 type txnMessage struct {
 	Txn [][]any `json:"txn"`
 }
 
 type server struct {
-	n      *maelstrom.Node
-	kv     *sync.Map
-	toSend *sync.Map
+	n  *maelstrom.Node
+	kv map[int]int
+	mu sync.RWMutex
 }
 
 func main() {
 	n := maelstrom.NewNode()
 	s := server{
-		n:      n,
-		kv:     &sync.Map{},
-		toSend: &sync.Map{},
+		n:  n,
+		kv: make(map[int]int),
+		mu: sync.RWMutex{},
 	}
 
 	n.Handle("txn", func(msg maelstrom.Message) error {
@@ -54,6 +53,12 @@ func (s *server) handleTxn(msg maelstrom.Message, isInternal bool) error {
 		return err
 	}
 	var results [][]any
+	var writeTxns [][]any
+
+	// Maintain lock for the entire transaction
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	for _, txn := range body.Txn {
 		var res []any
 		key := int(txn[1].(float64))
@@ -62,23 +67,22 @@ func (s *server) handleTxn(msg maelstrom.Message, isInternal bool) error {
 		} else if txn[0] == "w" {
 			val := int(txn[2].(float64))
 			res = s.handleWrite(key, val)
-			if !isInternal {
-				err = s.sendWrite(key, val)
-				if err != nil {
-					return err
-				}
-			}
+			writeTxns = append(writeTxns, txn)
 		}
 		results = append(results, res)
 	}
 	if isInternal {
 		return nil
 	}
+	err = s.sendWrites(writeTxns)
+	if err != nil {
+		return err
+	}
 	return s.n.Reply(msg, map[string]any{"type": "txn_ok", "txn": results})
 }
 
 func (s *server) handleRead(key int) []any {
-	val, ok := s.kv.Load(key)
+	val, ok := s.kv[key]
 	if !ok {
 		return []any{"r", key, nil}
 	}
@@ -86,16 +90,16 @@ func (s *server) handleRead(key int) []any {
 }
 
 func (s *server) handleWrite(key int, val int) []any {
-	s.kv.Store(key, val)
+	s.kv[key] = val
 	return []any{"w", key, val}
 }
 
-func (s *server) sendWrite(key int, val int) error {
+func (s *server) sendWrites(writeTxns [][]any) error {
 	for _, node := range s.n.NodeIDs() {
 		if s.n.ID() == node {
 			continue
 		}
-		err := s.n.Send(node, map[string]any{"type": "txn_internal", "txn": [][]any{{"w", key, val}}})
+		err := s.n.Send(node, map[string]any{"type": "txn_internal", "txn": writeTxns})
 		if err != nil {
 			return err
 		}
