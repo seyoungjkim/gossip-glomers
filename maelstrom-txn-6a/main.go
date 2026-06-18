75a4e427694f820a8b39cd03b322429c28524b52
Seyoung Kim <seyoungjkim@gmail.com>
2026-06-18 17:38:22 -0400

    correct 6a

--- maelstrom-txn-6a/main.go
+++ maelstrom-txn-6a/main.go
@@ -8,7 +8,10 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-// Run: ./../maelstrom/maelstrom test -w txn-rw-register --bin ~/go/bin/maelstrom-txn-6a --node-count 1 --time-limit 20 --rate 1000 --concurrency 2n --consistency-models read-uncommitted --availability total
+// Run: ./../maelstrom/maelstrom test -w txn-list-append --bin ~/go/bin/maelstrom-txn-6a --node-count 1 --time-limit 20 --rate 1000 --concurrency 2n --consistency-models read-uncommitted --availability total
+
+const readOp = "r"
+const writeOp = "append"
 
 type txnMessage struct {
 	Txn [][]any `json:"txn"`
@@ -16,18 +19,19 @@ type txnMessage struct {
 
 type server struct {
 	n  *maelstrom.Node
-	kv *sync.Map
+	kv map[int][]int
+	mu sync.Mutex
 }
 
 func main() {
 	n := maelstrom.NewNode()
 	s := server{
 		n:  n,
-		kv: &sync.Map{},
+		kv: make(map[int][]int),
+		mu: sync.Mutex{},
 	}
 
 	n.Handle("txn", func(msg maelstrom.Message) error {
-		// Unmarshal the message body
 		var body txnMessage
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
@@ -35,10 +39,11 @@ func main() {
 		var results [][]any
 		for _, txn := range body.Txn {
 			var res []any
-			if txn[0] == "r" {
-				res = s.handleRead(int(txn[1].(float64)))
-			} else if txn[0] == "w" {
-				res = s.handleWrite(int(txn[1].(float64)), int(txn[2].(float64)))
+			key := int(txn[1].(float64))
+			if txn[0] == readOp {
+				res = s.handleRead(key)
+			} else if txn[0] == writeOp {
+				res = s.handleWrite(key, int(txn[2].(float64)))
 			}
 			results = append(results, res)
 		}
@@ -51,14 +56,22 @@ func main() {
 }
 
 func (s *server) handleRead(key int) []any {
-	val, ok := s.kv.Load(key)
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	list, ok := s.kv[key]
 	if !ok {
-		return []any{"r", key, nil}
+		return []any{readOp, key, nil}
 	}
-	return []any{"r", key, val}
+	return []any{readOp, key, list}
 }
 
 func (s *server) handleWrite(key int, val int) []any {
-	s.kv.Store(key, val)
-	return []any{"w", key, val}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	_, ok := s.kv[key]
+	if !ok {
+		s.kv[key] = []int{}
+	}
+	s.kv[key] = append(s.kv[key], val)
+	return []any{writeOp, key, val}
 }
